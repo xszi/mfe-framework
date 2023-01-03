1b19e4d7e0925c63d8ff24080d39cd4d170199ba
jiuhuaw <jiuhuaw@126.com>
2023-01-03 10:36:48 +0800

    update：main.js

--- main/src/main.js
+++ main/src/main.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import 'normalize.css/normalize.css'
 import { MessageBox } from 'element-ui';
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import locale from 'element-ui/lib/locale/lang/en'
 import '@/styles/index.scss'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import '@/icons' // icon
+import '@/icons'
 
 Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
-Vue.prototype.$message =  MessageBox
+Vue.prototype.$message = MessageBox
 
 new Vue({
   el: '#app',
